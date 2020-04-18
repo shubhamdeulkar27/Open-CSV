ce50b98e43bc5d44e1257edd41df2a557f57ba16
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-04-18 11:37:26 +0530

    Added: CSVUsers Class which holds user data

---
+++ OpenCSV/CSVUsers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCSV
+{
+    /// <summary>
+    /// CSVUsers Class for setting and getting data.
+    /// </summary>
+    public class CSVUsers 
+    {
+        //Variales.
+        private string name;
+        private string email;
+        private string phoneNo;
+        private string country;
+
+        //Setters And Getters, to set and get the data.
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public string PhoneNo { get => phoneNo; set => phoneNo = value; }
+        public string Country { get => country; set => country = value; }
+    }
+}
--- OpenCSV/Program.cs
+++ OpenCSV/Program.cs
@@ -5,16 +5,44 @@ using System.Linq;
 
 namespace OpenCSV
 {
+    /// <summary>
+    /// Driver class.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main Function to parse CSV into CSVUsers Class.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //CSV file's path.
             string filepath = @"C:\Users\Shubham\source\repos\Open-CSV\OpenCSV\users.csv";
+
+            //List to store the CSVUsers Object.
+            List<CSVUsers> users = new List<CSVUsers>();
+            
+            //List to store the lines from file.
             List<string> lines = File.ReadAllLines(filepath).ToList();
+
+            //Loop to set the data to CSVUsers object and storing that object to List.
             foreach (string line in lines)
             {
-                Console.WriteLine(line);
+                string[] entries = line.Split(",");
+                CSVUsers user = new CSVUsers();
+                user.Name = entries[0];
+                user.Email = entries[1];
+                user.PhoneNo = entries[2];
+                user.Country = entries[3];
+                users.Add(user);
             }
+
+            //Loop to print the Data From CSVUsers type List.
+            foreach (CSVUsers user in users)
+            {
+                Console.WriteLine($"{user.Name} : {user.Email} : {user.PhoneNo} : {user.Country}");
+            }
+
         }
     }
 }
